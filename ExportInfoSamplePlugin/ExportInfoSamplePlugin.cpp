a500a29a195e13a246810bab9f7f11019fb96344
ZX_Diablo <yakut2004@bk.ru>
2017-11-05 21:59:09 +0500

    SceneExport plugin stub

--- ExportInfoSamplePlugin/ExportInfoSamplePlugin.cpp
+++ ExportInfoSamplePlugin/ExportInfoSamplePlugin.cpp
@@ -4,3 +4,57 @@ ClassDesc2* GetExportInfoSamplePluginDesc ()
 {
 	return nullptr;
 }
+
+int ExportInfoSamplePlugin::ExtCount ()
+{
+	return 1;
+}
+
+const MCHAR* ExportInfoSamplePlugin::Ext (int n)
+{
+	return _T("txt");
+}
+
+const MCHAR* ExportInfoSamplePlugin::LongDesc ()
+{
+	return _T("Export Info Sample Plugin");
+}
+
+const MCHAR* ExportInfoSamplePlugin::ShortDesc ()
+{
+	return _T("Export plugin");
+}
+
+const MCHAR* ExportInfoSamplePlugin::AuthorName ()
+{
+	return _T("ZX_Diablo");
+}
+
+const MCHAR* ExportInfoSamplePlugin::CopyrightMessage ()
+{
+	return _T("Copyright: ZX_Diablo");
+}
+
+const MCHAR* ExportInfoSamplePlugin::OtherMessage1 ()
+{
+	return _T("");
+}
+
+const MCHAR* ExportInfoSamplePlugin::OtherMessage2 ()
+{
+	return _T("");
+}
+
+unsigned int ExportInfoSamplePlugin::Version ()
+{
+	return PLUGIN_VERSION;
+}
+
+void ExportInfoSamplePlugin::ShowAbout (HWND hWnd)
+{
+}
+
+int ExportInfoSamplePlugin::DoExport (const MCHAR* name, ExpInterface* ei, Interface* i, BOOL suppressPrompts, DWORD options)
+{
+	return 0;
+}
--- ExportInfoSamplePlugin/ExportInfoSamplePlugin.h
+++ ExportInfoSamplePlugin/ExportInfoSamplePlugin.h
@@ -8,6 +8,30 @@
 
 #include "resource.h"
 
+#define PLUGIN_CLASS_ID Class_ID(0x64ca39bb, 0x5d2a67c4)
+#define PLUGIN_VERSION 100
+
 extern HINSTANCE hInstance;
 
 extern TCHAR* GetString (int id);
+
+class ExportInfoSamplePlugin : public SceneExport
+{
+public:
+	int ExtCount () override;
+	const MCHAR* Ext (int n) override;
+
+	const MCHAR* LongDesc () override;
+	const MCHAR* ShortDesc () override;
+
+	const MCHAR* AuthorName () override;
+	const MCHAR* CopyrightMessage () override;
+	const MCHAR* OtherMessage1 () override;
+	const MCHAR* OtherMessage2 () override;
+
+	unsigned int Version () override;
+
+	void ShowAbout (HWND hWnd) override;
+
+	int DoExport (const MCHAR* name, ExpInterface* ei, Interface* i, BOOL suppressPrompts = FALSE, DWORD options = 0) override;
+};
